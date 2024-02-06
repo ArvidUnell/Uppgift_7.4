2d4f011c9eba5b2582ded9306a21975eccb6a5bf
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-06 09:37:03 +0100

    Add project files.

---
+++ Uppgift_7.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_7_4
+{
+    class Program 
+    { 
+        static void Main(string[] args)
+        {
+            string[] ord = { "hej", "nej", "ja" };
+            Console.WriteLine(FinnsTeckentIAllaSträngar(ord,'j'));
+            Console.WriteLine(FinnsTeckentIAllaSträngar(ord, 'e'));
+        }
+        static bool FinnsTeckentIAllaSträngar(string[] strängArr, char tecken)
+        {
+            foreach (string sträng in strängArr)
+            {
+                if (!sträng.Contains(tecken))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
